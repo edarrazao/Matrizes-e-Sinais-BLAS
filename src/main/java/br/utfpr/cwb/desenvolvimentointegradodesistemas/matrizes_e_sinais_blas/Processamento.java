eee8972aab09e60ffca6634091104dec8093db29
eduponto21 <eduponto21@gmail.com>
2021-10-22 15:33:45 -0300

    Criado classe de processamentos

---
+++ src/main/java/br/utfpr/cwb/desenvolvimentointegradodesistemas/matrizes_e_sinais_blas/Processamento.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.utfpr.cwb.desenvolvimentointegradodesistemas.matrizes_e_sinais_blas;
+
+/**
+ *
+ * @author edupo
+ */
+public class Processamento {
+
+    //Construtor
+    public Processamento() {
+
+    }
+
+    //Codificar um protótipo com o Algoritmo CGNR. Validar os resultados com os dados experimentais.
+    //Medir o tempo total de execução e o consumo de recursos como memória e ocupação de CPU.
+    public void algoritmoCGNR() {
+        /**
+        Entrada: H e g
+        Saída: f
+        g - Vetor de sinal
+        H - Matriz de modelo
+        f - Imagem
+        S - Número de amostras do sinal
+        N - Número de elementos sensores
+        Cálculo do fator de redução (c)
+        Cálculo do coeficiente de regularização (λ)
+        Cálculo do erro (ϵ)
+        Cálculo do ganho de sinal (γ)
+        * Calculo de vetores norma 2 euclidiana
+        * Matriz transposta (elevado a T)
+        **/
+        
+        
+        
+        
+
+    }
+
+}
