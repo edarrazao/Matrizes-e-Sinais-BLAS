aaf2578997dba5c411c244962258f5e393712142
Daniel Vieira <dvieira@alunos.utfpr.edu.br>
2021-10-22 18:53:15 -0300

    arquitetura multicamada 5-layers aplicada

--- src/main/java/br/utfpr/cwb/sinais/core/Main.java
+++ src/main/java/br/utfpr/cwb/sinais/core/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.utfpr.cwb.sinais;
+package br.utfpr.cwb.sinais.core;
 
 import br.utfpr.cwb.sinais.api.Program;
 import br.utfpr.cwb.sinais.services.Services;
